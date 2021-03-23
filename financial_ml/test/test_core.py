32febbe73d581b8a03cf0baabe2389e46c91c82a
hjimenezdia <hjimenezdiaz429@gmail.com>
2021-03-23 02:13:17 +0000

    Se agregan test al metodo clone

--- financial_ml/test/test_core.py
+++ financial_ml/test/test_core.py
@@ -21,21 +21,27 @@ class TimeSerieTest(unittest.TestCase):
         return pd.read_parquet(repo + '/data/dummy.parquet')
 
     def test_builder_df2timeseries(self):
+        #give
         dummy_df = self.read_dummy_df()
-        
         builder = DF2TimeSerie(pd.Timedelta('1d'))
+
+        #when: Se construlle objeto Timeseria partiendo de un DataFrame
         timeserie = builder.build(dummy_df)
+
+        #then
         self.assertTrue(isinstance(timeserie, TimeSerie))
         
     def test_transformation_differentiator(self):
+        #given
         dummy_df = self.read_dummy_df()
-        
         builder = DF2TimeSerie(pd.Timedelta('1d'))
         timeserie = builder.build(dummy_df)
         
+        #when: Diferenciar un objeto de tipo Timeserie, produce un objeto timeserie
         differentiator = Differentitator()
         timeserie_diff = differentiator.transform(timeserie)
         
+        #then
         self.assertTrue(isinstance(timeserie_diff, TimeSerie))
         
     def test_to_df(self):
@@ -43,7 +49,7 @@ class TimeSerieTest(unittest.TestCase):
         dummy_df = self.read_dummy_df()
         builder = DF2TimeSerie(pd.Timedelta('1d'))
         
-        #when
+        #when: Verifica que el objeto TImeserie esta soportado sobre un dataframe de pandas
         timeserie = builder.build(dummy_df)
         df = timeserie.to_df()
         
@@ -58,7 +64,7 @@ class TimeSerieTest(unittest.TestCase):
         timeserie = builder.build(dummy_df)
         callback = lambda df:df
         
-        #when
+        #when: Tranforma la función, asignandole otra función
         timeSerie_traforme = timeserie.apply(callback)
         df = timeSerie_traforme.to_df()
         
@@ -66,7 +72,20 @@ class TimeSerieTest(unittest.TestCase):
         self.assertTrue(isinstance(timeSerie_traforme,TimeSerie))
         self.assertEqual(dummy_df.shape[0],df.shape[0])
 
+    def test_clone(self):
+        #given
+        dummy_df = self.read_dummy_df()
+        builder = DF2TimeSerie(pd.Timedelta('1d'))
+        timeserie = builder.build(dummy_df)
+
+        #when: Crea otra copia de la data en memoria
+        timeserie_clone = timeserie.clone()
+        df = timeserie_clone.to_df()
+
+        #then
+        self.assertTrue(isinstance(timeserie_clone,TimeSerie))
+        self.assertTrue(isinstance(df,pd.DataFrame))
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
-
 
